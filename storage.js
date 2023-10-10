4c5048fc6716f2ddb91e077d2ad5b2debc1c2ac7
arundhati-shreya <arundhati0426@gmail.com>
2023-10-10 23:40:04 +0530

    Dom manipulation 9

--- storage.js
+++ storage.js
@@ -3,19 +3,17 @@ const myForm = document.querySelector('#my-form');
 const nameInput = document.querySelector('#name');
 const emailInput = document.querySelector('#email');
 const msg = document.querySelector('.msg');
-
 // Listen for form submit
 myForm.addEventListener('submit', onSubmit);
-
 function onSubmit(e) {
   e.preventDefault();
-
   if (nameInput.value === '' || emailInput.value === '') {
     msg.classList.add('error');
     msg.innerHTML = 'Please enter all fields';
     setTimeout(() => msg.remove(), 3000);
   } else {
     // Store user details in local storage
+    // Create an object to store user details
     const userDetails = {
       name: nameInput.value,
       email: emailInput.value,
@@ -25,8 +23,31 @@ function onSubmit(e) {
     // Store the JSON string in local storage
     localStorage.setItem('userDetails', userDetailsJSON);
 
+    // Call a separate function to add user details to local storage
+    addToLocalStorage(userDetails);
+
     // Clear fields
     nameInput.value = '';
     emailInput.value = '';
   }
+}
+
+// Function to add new user details to local storage
+function addToLocalStorage(userDetails) {
+  // Check if there are existing user details in local storage
+  let storedUserDetails = JSON.parse(localStorage.getItem('userDetails')) || [];
+
+  // Make sure storedUserDetails is an array
+  if (!Array.isArray(storedUserDetails)) {
+    storedUserDetails = [];
+  }
+
+  // Add the new user details to the existing array
+  storedUserDetails.push(userDetails);
+
+  // Convert the array to a JSON string
+  const userDetailsJSON = JSON.stringify(storedUserDetails);
+
+  // Store the JSON string in local storage
+  localStorage.setItem('userDetails', userDetailsJSON);
 }
